8c6e534360b0c80be9fe05dd400a7f3462978255
Bulent Gorkem <bulent.gorkem@bg-mbp.local>
2021-06-09 21:59:45 +0100

    Add socket console, subscribe to topic prettier config

--- src/__tests__/AppComponent.test.js
+++ src/__tests__/AppComponent.test.js
@@ -1,45 +1,22 @@
-
-import React from 'react'
+import React from 'react';
 import AppComponent from '../AppComponent';
 
-import { render, waitFor, screen, cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import { render, waitFor, screen, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
+describe('AppComponent test', () => {
+  beforeEach(() => {
+    render(<AppComponent />);
+  });
 
-describe ('AppComponent test', ()=>{
+  afterEach(() => {
+    cleanup();
+  });
 
-    let comp = null;
-
-    beforeEach( ()=>{
-         comp = render(<AppComponent/>)      
-    })
-
-    afterEach( ()=>{
-        comp = null;
-        cleanup()
-    })
-
-    test('it should render the header', async ()=>{
-       
-        const header = await waitFor(() => screen.getByTestId('header'))
-        expect (header).toHaveTextContent('This is the count: 0');
-        
-    })
-
-    test('it should contain the right link', async ()=> {
-        const link = comp.getByTestId('link')
-        expect (link).toBeInTheDocument();
-        expect (link).toHaveAttribute('href', 'http://www.bbc.com')
-        expect (link).toHaveTextContent('Link to BBC')
-    })
-
-    test('it should increase the count', async ()=> {
-       
-        const header = screen.getByTestId('header')
-        const button= comp.getByTestId('button');
-        button.click();
-
-        expect (header).toHaveTextContent('This is the count: 1');
-
-    })
-})
+  test('it should render the console', async () => {
+    const socketConsole = await waitFor(() =>
+      screen.getByTestId('socket-console')
+    );
+    expect(socketConsole).toBeInTheDocument();
+  });
+});
--- src/library.ts
+++ src/library.ts
@@ -1,4 +1,37 @@
+import { SessionEventCode, SolclientFactory } from 'solclientjs';
+import config from './config';
+
 export function myAdder(x: number, y: number): number {
   return x + y;
 }
 
+/**
+ * Connects to Solace PubSub+ on Web, subscribes to prices topic
+ * @param logger logging callback
+ */
+export function connect(log: (message: string) => void): void {
+  SolclientFactory.init({});
+  log('initialized');
+
+  const subscriber = SolclientFactory.createSession(config);
+  try {
+    subscriber.connect();
+  } catch (error) {
+    log(error);
+  }
+
+  subscriber.on(SessionEventCode.UP_NOTICE, function () {
+    log('connected, subscribing topic: T/Crypto/prices');
+    const t = SolclientFactory.createTopicDestination('T/Crypto/prices');
+    subscriber.subscribe(t, true, 'T/Crypto/prices');
+  });
+
+  subscriber.on(SessionEventCode.MESSAGE, function (message) {
+    const m = message.getSdtContainer().getValue() || '';
+    log('Received message:' + m);
+  });
+
+  subscriber.on(SessionEventCode.SUBSCRIPTION_OK, function () {
+    log('Subscription ok');
+  });
+}
